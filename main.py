ecd11facdf0b853a70685fbf65312f88d43527c8
avi372 <vaibhavjaijan78@gmail.com>
2024-10-14 13:36:03 +0000

    Pending changes exported from your codespace

--- main.py
+++ main.py
@@ -38,11 +38,11 @@ def install_requirements():
 install_requirements()
 
 # 🎛️ Telegram API token (replace with your actual token)
-TOKEN = '7424479555:AAGtNAbqXax_b3b--QnGYi5_XwBbQNw'
+TOKEN = '7872529656:AAHVhNDavupyCRzpMi5EpFRYKqNe8TDMvw4'
 bot = telebot.TeleBot(TOKEN, threaded=False)
 
 # 🛡️ List of authorized user IDs (replace with actual IDs)
-AUTHORIZED_USERS = [5588464519]
+AUTHORIZED_USERS = [6850753246,5715632016]
 
 # 🌐 Global dictionary to keep track of user attacks
 user_attacks = {}
